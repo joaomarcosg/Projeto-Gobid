95089814e3d73889999b7115ce8c429abe7a15cb
joaomarcosg <joaomarcos.ipms@gmail.com>
2025-07-21 16:39:28 -0300

    FEAT: adds UserStore interface to the store

--- internal/store/user_store.go
+++ internal/store/user_store.go
@@ -1,6 +1,7 @@
 package store
 
 import (
+	"context"
 	"time"
 
 	"github.com/google/uuid"
@@ -14,4 +15,11 @@ type User struct {
 	Bio          string    `json:"bio"`
 	CreatedAt    time.Time `json:"created_at"`
 	UpdatedAt    time.Time `json:"updated_at"`
+}
+
+type UserStore interface {
+	CreateUser(ctx context.Context, userName, email, password, bio string) (uuid.UUID, error)
+	AuthenticateUser(ctx context.Context, email, password string) (uuid.UUID, error)
+	GetUserByEmail(ctx context.Context, email string) (uuid.UUID, error)
+	GetUserById(ctx context.Context, id uuid.UUID) (User, error)
 }
