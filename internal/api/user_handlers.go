14b62c0d56dd2e9bb0a1efd9c2db03f2ed270d7b
joaomarcosg <joaomarcos.ipms@gmail.com>
2025-07-22 17:16:12 -0300

    FEAT: adds minimal functionality to the handler handleSignupUser

--- internal/api/user_handlers.go
+++ internal/api/user_handlers.go
@@ -1,7 +1,25 @@
 package api
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/joaomarcosg/Projeto-Gobid/internal/jsonutils"
+	"github.com/joaomarcosg/Projeto-Gobid/internal/usecase/user"
+)
 
 func (api *Api) handleSignupUser(w http.ResponseWriter, r *http.Request) {
+
+	data, problems, err := jsonutils.DecodeValidJson[user.CreateUserReq](r)
+	if err != nil {
+		_ = jsonutils.EncodeJson(w, r, http.StatusUnprocessableEntity, problems)
+		return
+	}
+
+	_ = jsonutils.EncodeJson(w, r, http.StatusCreated, map[string]any{
+		"user_name": data.UserName,
+		"email":     data.Email,
+		"password":  data.Password,
+		"bio":       data.Bio,
+	})
 
 }
