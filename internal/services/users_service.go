e4e1c5f7d008f26e85b8a7d50f0a8736e218ce5e
joaomarcosg <joaomarcos.ipms@gmail.com>
2025-07-22 18:15:05 -0300

    FEAT: adds createUser method

---
+++ internal/services/users_service.go
@@ -0,0 +1,34 @@
+package services
+
+import (
+	"context"
+
+	"github.com/google/uuid"
+	"github.com/joaomarcosg/Projeto-Gobid/internal/store"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type UserService struct {
+	Store store.UserStore
+}
+
+func NewUserService(store store.UserStore) *UserService {
+	return &UserService{
+		Store: store,
+	}
+}
+
+func (us *UserService) CreateUser(
+	ctx context.Context,
+	userName,
+	email,
+	password,
+	bio string,
+) (uuid.UUID, error) {
+
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), 12)
+	if err != nil {
+		return uuid.UUID{}, err
+	}
+
+}
