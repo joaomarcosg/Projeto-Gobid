4e97ca0e27f1a8dae02b60d475e749cd4fdce947
joaomarcosg <joaomarcos.ipms@gmail.com>
2025-07-23 12:15:25 -0300

    FEAT: adds settings to upload the server

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,5 +1,53 @@
 package main
 
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/joaomarcosg/Projeto-Gobid/internal/api"
+	"github.com/joaomarcosg/Projeto-Gobid/internal/services"
+	"github.com/joho/godotenv"
+)
+
 func main() {
+
+	if err := godotenv.Load(); err != nil {
+		panic(err)
+	}
+
+	ctx := context.Background()
+	pool, err := pgxpool.New(ctx, fmt.Sprintf("user=%s password=%s host=%s port=%s dbname=%s",
+		os.Getenv("GOBID_DATABASE_USER"),
+		os.Getenv("GOBID_DATABASE_PASSWORD"),
+		os.Getenv("GOBID_DATABASE_HOST"),
+		os.Getenv("GOBID_DATABASE_PORT"),
+		os.Getenv("GOBID_DATABASE_NAME"),
+	))
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer pool.Close()
+
+	if err := pool.Ping(ctx); err != nil {
+		panic(err)
+	}
+
+	api := api.Api{
+		Router:      chi.NewMux(),
+		UserService: *services.NewUserService(pool),
+	}
+
+	api.BindRoutes()
+
+	fmt.Println("Starting server on port :3080")
+	if err := http.ListenAndServe(":3080", api.Router); err != nil {
+		panic(err)
+	}
 
 }
