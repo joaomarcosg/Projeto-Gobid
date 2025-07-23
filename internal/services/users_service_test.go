188affb206bcd5ca878684b5c887b7ee2782c3c3
joaomarcosg <joaomarcos.ipms@gmail.com>
2025-07-23 10:24:30 -0300

    TEST: adds UserStore interface methods for testing

--- internal/services/users_service_test.go
+++ internal/services/users_service_test.go
@@ -2,23 +2,38 @@ package services
 
 import (
 	"context"
-	"time"
+	"testing"
 
 	"github.com/google/uuid"
 	"github.com/joaomarcosg/Projeto-Gobid/internal/store"
+	"github.com/stretchr/testify/assert"
 )
 
 type MockUserStore struct{}
 
-func (m *MockUserStore) CreateUser(ctx context.Context, userName, email, password, bio string) (store.User, error) {
+func (m *MockUserStore) CreateUser(ctx context.Context, userName, email, password, bio string) (uuid.UUID, error) {
 	id, _ := uuid.Parse("123e4567-e89b-12d3-a456-426614174000")
-	return store.User{
-		ID:           id,
-		UserName:     userName,
-		Email:        email,
-		PasswordHash: []byte("mocked_hash_password"),
-		Bio:          bio,
-		CreatedAt:    time.Now(),
-		UpdatedAt:    time.Now(),
-	}, nil
+	return id, nil
+}
+
+func (m *MockUserStore) AuthenticateUser(ctx context.Context, email, password string) (uuid.UUID, error) {
+	return uuid.UUID{}, nil
+}
+
+func (m *MockUserStore) GetUserByEmail(ctx context.Context, email string) (uuid.UUID, error) {
+	return uuid.UUID{}, nil
+}
+
+func (m *MockUserStore) GetUserById(ctx context.Context, id uuid.UUID) (store.User, error) {
+	return store.User{}, nil
+}
+
+func TestCreateUser(t *testing.T) {
+	mockStore := MockUserStore{}
+	userService := NewUserService(&mockStore)
+
+	id, err := userService.CreateUser(context.Background(), "Marcos", "marcos@gmail.com", "Senha123456", "Hellom world!")
+
+	assert.NoError(t, err)
+
 }
