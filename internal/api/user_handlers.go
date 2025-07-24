08a7c43214e7c0de8f57f87f2c083c01480ef503
joaomarcosg <joaomarcos.ipms@gmail.com>
2025-07-24 09:50:22 -0300

    FEAT: finishes the features of the handleSignupUser handler

--- internal/api/user_handlers.go
+++ internal/api/user_handlers.go
@@ -1,25 +1,42 @@
 package api
 
 import (
+	"errors"
 	"net/http"
 
 	"github.com/joaomarcosg/Projeto-Gobid/internal/jsonutils"
+	"github.com/joaomarcosg/Projeto-Gobid/internal/services"
 	"github.com/joaomarcosg/Projeto-Gobid/internal/usecase/user"
 )
 
 func (api *Api) handleSignupUser(w http.ResponseWriter, r *http.Request) {
 
 	data, problems, err := jsonutils.DecodeValidJson[user.CreateUserReq](r)
+
 	if err != nil {
 		_ = jsonutils.EncodeJson(w, r, http.StatusUnprocessableEntity, problems)
 		return
 	}
 
+	id, err := api.UserService.CreateUser(
+		r.Context(),
+		data.UserName,
+		data.Email,
+		data.Password,
+		data.Bio,
+	)
+
+	if err != nil {
+		if errors.Is(err, services.ErrDuplicatedEmailOrUserName) {
+			_ = jsonutils.EncodeJson(w, r, http.StatusUnprocessableEntity, map[string]any{
+				"error": "email or username already exists",
+			})
+			return
+		}
+	}
+
 	_ = jsonutils.EncodeJson(w, r, http.StatusCreated, map[string]any{
-		"user_name": data.UserName,
-		"email":     data.Email,
-		"password":  data.Password,
-		"bio":       data.Bio,
+		"user_id": id,
 	})
 
 }
