ef3e06e148525eccc7eb844c54d6c37daf373872
joaomarcosg <joaomarcos.ipms@gmail.com>
2025-07-25 12:30:31 -0300

    CHORE: fixes .env variables syntax

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -24,11 +24,11 @@ func main() {
 
 	ctx := context.Background()
 	pool, err := pgxpool.New(ctx, fmt.Sprintf("user=%s password=%s host=%s port=%s dbname=%s",
-		os.Getenv("GO_BID_DATABASE_USER"),
-		os.Getenv("GO_BID_DATABASE_PASSWORD"),
-		os.Getenv("GO_BID_DATABASE_HOST"),
-		os.Getenv("GO_BID_DATABASE_PORT"),
-		os.Getenv("GO_BID_DATABASE_NAME"),
+		os.Getenv("GOBID_DATABASE_USER"),
+		os.Getenv("GOBID_DATABASE_PASSWORD"),
+		os.Getenv("GOBID_DATABASE_HOST"),
+		os.Getenv("GOBID_DATABASE_PORT"),
+		os.Getenv("GOBID_DATABASE_NAME"),
 	))
 
 	if err != nil {
