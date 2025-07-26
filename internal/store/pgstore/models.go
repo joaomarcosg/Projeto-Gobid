5c241d659ce2e761670ce24a5db0beaa8ce82554
joaomarcosg <joaomarcos.ipms@gmail.com>
2025-07-26 13:48:09 -0300

    FEAT: adds the methods of the products generated by SQLC

--- internal/store/pgstore/models.go
+++ internal/store/pgstore/models.go
@@ -10,6 +10,24 @@ import (
 	"github.com/google/uuid"
 )
 
+type Product struct {
+	ID          uuid.UUID `json:"id"`
+	SellerID    uuid.UUID `json:"seller_id"`
+	ProductName string    `json:"product_name"`
+	Description string    `json:"description"`
+	Baseprice   float64   `json:"baseprice"`
+	AuctionEnd  time.Time `json:"auction_end"`
+	IsSold      bool      `json:"is_sold"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
+}
+
+type Session struct {
+	Token  string    `json:"token"`
+	Data   []byte    `json:"data"`
+	Expiry time.Time `json:"expiry"`
+}
+
 type User struct {
 	ID           uuid.UUID `json:"id"`
 	UserName     string    `json:"user_name"`
