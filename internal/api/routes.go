46643f2c7a20fde8820e903effd7ad4a9919f808
joaomarcosg <joaomarcos.ipms@gmail.com>
2025-07-26 12:28:04 -0300

    FEAT: adds middlewares from the gorilla/csrf package and others middlewares

--- internal/api/routes.go
+++ internal/api/routes.go
@@ -1,9 +1,22 @@
 package api
 
-import "github.com/go-chi/chi/v5"
+import (
+	"os"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/gorilla/csrf"
+)
 
 func (api *Api) BindRoutes() {
-	api.Router.Use(api.Sessions.LoadAndSave)
+	api.Router.Use(middleware.RequestID, middleware.Recoverer, middleware.logger, api.Sessions.LoadAndSave)
+
+	csrfMiddleware := csrf.Protect(
+		[]byte(os.Getenv("GOBID_CSRF_KEY")),
+		csrf.Secure(false), //DEV ONLY
+	)
+
+	api.Router.Use(csrfMiddleware)
 
 	api.Router.Route("/api", func(r chi.Router) {
 		r.Route("/v1", func(r chi.Router) {
