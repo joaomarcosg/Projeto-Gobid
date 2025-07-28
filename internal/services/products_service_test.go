ecf8ee9d8d04242ade55111e45953745015d315f
joaomarcosg <joaomarcos.ipms@gmail.com>
2025-07-28 12:10:39 -0300

    TEST: adds CreateProduct test

--- internal/services/products_service_test.go
+++ internal/services/products_service_test.go
@@ -1,1 +1,48 @@
 package services
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/joaomarcosg/Projeto-Gobid/internal/store"
+	"github.com/stretchr/testify/assert"
+)
+
+type MockProductStore struct{}
+
+func (m *MockProductStore) CreateProduct(
+	ctx context.Context,
+	sellerID uuid.UUID,
+	productName,
+	description string,
+	baseprice float64,
+	auctionEnd time.Time,
+) (uuid.UUID, error) {
+	productID := uuid.New()
+	return productID, nil
+}
+
+func (m *MockProductStore) GetProductById(ctx context.Context, productID uuid.UUID) (store.Product, error) {
+	return store.Product{}, nil
+}
+
+func TestCreateProduct(t *testing.T) {
+
+	mockStore := MockProductStore{}
+	productService := NewProductService(&mockStore)
+
+	ctx := context.Background()
+	sellerID := uuid.New()
+	productName := "Produto de teste"
+	description := "Descrição do produto"
+	baseprice := 99.99
+	auctionEnd := time.Now().Add(2 * time.Hour)
+
+	productID, err := productService.CreateProduct(ctx, sellerID, productName, description, baseprice, auctionEnd)
+
+	assert.NoError(t, err)
+	assert.NotEqual(t, uuid.Nil, productID)
+
+}
