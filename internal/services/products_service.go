00970c3e9a409d18dfe6cc122b0d17dfd9728f19
joaomarcosg <joaomarcos.ipms@gmail.com>
2025-07-28 11:19:00 -0300

    FEAT: adds CreateProduct method

--- internal/services/products_service.go
+++ internal/services/products_service.go
@@ -1,1 +1,44 @@
 package services
+
+import (
+	"context"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/joaomarcosg/Projeto-Gobid/internal/store"
+)
+
+type ProductService struct {
+	Store store.ProductStore
+}
+
+func NewProductService(store store.ProductStore) *ProductService {
+	return &ProductService{
+		Store: store,
+	}
+}
+
+func (ps *ProductService) CreateProduct(
+	ctx context.Context,
+	sellerID uuid.UUID,
+	productName,
+	description string,
+	baseprice float64,
+	auctionEnd time.Time,
+) (uuid.UUID, error) {
+
+	productID, err := ps.Store.CreateProduct(
+		ctx,
+		sellerID,
+		productName,
+		description,
+		baseprice,
+		auctionEnd,
+	)
+	if err != nil {
+		return uuid.UUID{}, err
+	}
+
+	return productID, nil
+
+}
