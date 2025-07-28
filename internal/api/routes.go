32b5b89888147521ab69e3e94ea2961c67f45f2a
joaomarcosg <joaomarcos.ipms@gmail.com>
2025-07-28 12:42:49 -0300

    FIX: adjusts handleLogoutUser route in a group

--- internal/api/routes.go
+++ internal/api/routes.go
@@ -24,7 +24,12 @@ func (api *Api) BindRoutes() {
 			r.Route("/users", func(r chi.Router) {
 				r.Post("/signup", api.handleSignupUser)
 				r.Post("/loginuser", api.handleLoginUser)
-				r.With(api.AuthMiddleware).Post("/logoutuser", api.handleLogoutUser)
+
+				r.Group(func(r chi.Router) {
+					r.Use(api.AuthMiddleware)
+					r.Post("/logout", api.handleLogoutUser)
+				})
+
 			})
 		})
 	})
