695f9ec7ec1e64cd7d7ad244df9b3f900dfe8844
joaomarcosg <joaomarcos.ipms@gmail.com>
2025-07-29 19:13:23 -0300

    FEAT: adds BidService type and its constructor

--- internal/services/bids_service.go
+++ internal/services/bids_service.go
@@ -1,1 +1,13 @@
 package services
+
+import "github.com/joaomarcosg/Projeto-Gobid/internal/store"
+
+type BidService struct {
+	Store store.BidStore
+}
+
+func NewBidService(store store.BidStore) *BidService {
+	return &BidService{
+		Store: store,
+	}
+}
