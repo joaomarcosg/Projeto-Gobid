a5812f5e2395d98b330987357183f4649cf58107
joaomarcosg <joaomarcos.ipms@gmail.com>
2025-07-29 13:12:14 -0300

    FEAT: implements methods of Bid interface

--- internal/store/pgstore/pg_bid_store.go
+++ internal/store/pgstore/pg_bid_store.go
@@ -1,7 +1,11 @@
 package pgstore
 
 import (
+	"context"
+
+	"github.com/google/uuid"
 	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/joaomarcosg/Projeto-Gobid/internal/store"
 )
 
 type PGBidStore struct {
@@ -14,4 +18,58 @@ func NewPGBidStore(pool *pgxpool.Pool) *PGBidStore {
 		Queries: New(pool),
 		Pool:    pool,
 	}
+}
+
+func (pgb *PGBidStore) CreateBid(
+	ctx context.Context,
+	productID,
+	bidderID uuid.UUID,
+	bidAmount float64,
+) (store.Bid, error) {
+
+	bid, err := pgb.Queries.CreateBid(ctx, CreateBidParams{
+		ProductID: productID,
+		BidderID:  bidderID,
+		BidAmount: bidAmount,
+	})
+
+	if err != nil {
+		return store.Bid{}, err
+	}
+
+	return store.Bid{
+		ID:        bid.ID,
+		ProductID: bid.ProductID,
+		BidderID:  bid.BidderID,
+		BidAmount: bid.BidAmount,
+	}, nil
+
+}
+
+func (pgb *PGBidStore) GetBidsByProduct(ctx context.Context, productID uuid.UUID) ([]Bid, error) {
+
+	bids, err := pgb.Queries.GetBidsByProduct(ctx, productID)
+	if err != nil {
+		return nil, err
+	}
+
+	return bids, nil
+
+}
+
+func (pgb *PGBidStore) GetHighestBidByProductId(ctx context.Context, productID uuid.UUID) (store.Bid, error) {
+
+	bid, err := pgb.Queries.GetHighestBidByProductId(ctx, productID)
+	if err != nil {
+		return store.Bid{}, err
+	}
+
+	return store.Bid{
+		ID:        bid.ID,
+		ProductID: bid.ProductID,
+		BidderID:  bid.BidderID,
+		BidAmount: bid.BidAmount,
+		CreatedAt: bid.CreatedAt,
+	}, nil
+
 }
