08818d7609047f9b8ff1cefb36a732fa62a875bb
joaomarcosg <joaomarcos.ipms@gmail.com>
2025-07-29 18:38:18 -0300

    TEST: adds GetProductById method test

--- internal/services/products_service_test.go
+++ internal/services/products_service_test.go
@@ -25,7 +25,14 @@ func (m *MockProductStore) CreateProduct(
 }
 
 func (m *MockProductStore) GetProductById(ctx context.Context, productID uuid.UUID) (store.Product, error) {
-	return store.Product{}, nil
+	return store.Product{
+		ID:          productID,
+		SellerID:    uuid.New(),
+		ProductName: "Producto de teste",
+		Description: "Descrição do produto",
+		Baseprice:   99.99,
+		AuctionEnd:  time.Now().Add(2 * time.Hour),
+	}, nil
 }
 
 func TestCreateProduct(t *testing.T) {
@@ -44,5 +51,20 @@ func TestCreateProduct(t *testing.T) {
 
 	assert.NoError(t, err)
 	assert.NotEqual(t, uuid.Nil, productID)
+
+}
+
+func TestGetProductById(t *testing.T) {
+
+	mockStore := MockProductStore{}
+	productService := NewProductService(&mockStore)
+
+	ctx := context.Background()
+	productID := uuid.New()
+
+	product, err := productService.Store.GetProductById(ctx, productID)
+
+	assert.NoError(t, err)
+	assert.Equal(t, productID, product.ID)
 
 }
