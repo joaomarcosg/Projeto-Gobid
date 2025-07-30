56ff160698fb835b482766014b46b833fe854649
joaomarcosg <joaomarcos.ipms@gmail.com>
2025-07-30 12:44:07 -0300

    FEAT: adds GetProductById method

--- internal/services/products_service.go
+++ internal/services/products_service.go
@@ -2,9 +2,11 @@ package services
 
 import (
 	"context"
+	"errors"
 	"time"
 
 	"github.com/google/uuid"
+	"github.com/jackc/pgx/v5"
 	"github.com/joaomarcosg/Projeto-Gobid/internal/store"
 )
 
@@ -40,5 +42,28 @@ func (ps *ProductService) CreateProduct(
 	}
 
 	return productID, nil
+
+}
+
+func (ps *ProductService) GetProductById(ctx context.Context, productID uuid.UUID) (store.Product, error) {
+
+	product, err := ps.Store.GetProductById(ctx, productID)
+	if err != nil {
+		if errors.Is(err, pgx.ErrNoRows) {
+			return store.Product{}, err
+		}
+	}
+
+	return store.Product{
+		ID:          product.ID,
+		SellerID:    product.ID,
+		ProductName: product.ProductName,
+		Description: product.Description,
+		Baseprice:   product.Baseprice,
+		AuctionEnd:  product.AuctionEnd,
+		IsSold:      product.IsSold,
+		CreatedAt:   product.CreatedAt,
+		UpdatedAt:   product.UpdatedAt,
+	}, nil
 
 }
