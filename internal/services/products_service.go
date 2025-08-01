f48cd5a0a81855e86a95e1fad28a1396aad14fb6
joaomarcosg <joaomarcos.ipms@gmail.com>
2025-08-01 16:35:19 -0300

    FEAT: adds a custom error

--- internal/services/products_service.go
+++ internal/services/products_service.go
@@ -45,13 +45,16 @@ func (ps *ProductService) CreateProduct(
 
 }
 
+var ErrProductNotFound = errors.New("product not found")
+
 func (ps *ProductService) GetProductById(ctx context.Context, productID uuid.UUID) (store.Product, error) {
 
 	product, err := ps.Store.GetProductById(ctx, productID)
 	if err != nil {
 		if errors.Is(err, pgx.ErrNoRows) {
-			return store.Product{}, err
+			return store.Product{}, ErrProductNotFound
 		}
+		return store.Product{}, err
 	}
 
 	return store.Product{
