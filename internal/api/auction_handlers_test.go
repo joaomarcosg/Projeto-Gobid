9b7dfd2c70fbe3f9263125c9a26ad84c33651b2a
joaomarcosg <joaomarcos.ipms@gmail.com>
2025-08-01 17:19:35 -0300

    TEST: adds handleSubscribeToAuction with a valid UUID

--- internal/api/auction_handlers_test.go
+++ internal/api/auction_handlers_test.go
@@ -2,13 +2,45 @@ package api
 
 import (
 	"context"
+	"encoding/gob"
 	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
 
+	"github.com/alexedwards/scs/v2"
+	"github.com/alexedwards/scs/v2/memstore"
 	"github.com/go-chi/chi/v5"
+	"github.com/google/uuid"
+	"github.com/joaomarcosg/Projeto-Gobid/internal/services"
+	"github.com/joaomarcosg/Projeto-Gobid/internal/store"
 )
+
+type mockProductStoreHandler struct{}
+
+func (m *mockProductStoreHandler) CreateProduct(
+	ctx context.Context,
+	sellerID uuid.UUID,
+	productName,
+	description string,
+	baseprice float64,
+	auctionEnd time.Time,
+) (uuid.UUID, error) {
+	productID := uuid.New()
+	return productID, nil
+}
+
+func (m *mockProductStoreHandler) GetProductById(ctx context.Context, productID uuid.UUID) (store.Product, error) {
+	return store.Product{
+		ID:          productID,
+		SellerID:    uuid.New(),
+		ProductName: "Producto de teste",
+		Description: "Descrição do produto",
+		Baseprice:   99.99,
+		AuctionEnd:  time.Now().Add(2 * time.Hour),
+	}, nil
+}
 
 func TestHandleSubscribeToAuctionWithInvalidUUID(t *testing.T) {
 
@@ -40,6 +72,38 @@ func TestHandleSubscribeToAuctionWithInvalidUUID(t *testing.T) {
 
 	if resBody["message"] != "invalid product id - must be a valid uuid" {
 		t.Errorf("expected 'invalid product id - must be a valid uuid, got %q", resBody["message"])
+	}
+
+}
+
+func TestHandleSubscribeToAuctionWithValidUUID(t *testing.T) {
+
+	gob.Register(uuid.UUID{})
+
+	sessionManager := scs.New()
+	sessionManager.Store = memstore.New()
+
+	api := Api{
+		ProductService: *services.NewProductService(&mockProductStoreHandler{}),
+		Sessions:       sessionManager,
+	}
+
+	validUUID := uuid.New().String()
+
+	req := httptest.NewRequest("GET", "/api/v1/products/ws/subscribe/"+validUUID, nil)
+
+	routeContext := chi.NewRouteContext()
+	routeContext.URLParams.Add("product_id", validUUID)
+	req = req.WithContext(context.WithValue(req.Context(), chi.RouteCtxKey, routeContext))
+
+	rec := httptest.NewRecorder()
+	handler := http.HandlerFunc(api.handleSubscribeToAuction)
+	handler.ServeHTTP(rec, req)
+
+	t.Logf("Rec body %s\n", rec.Body.Bytes())
+
+	if rec.Code == http.StatusBadRequest {
+		t.Fatalf("invalid operation, expected a different code than %q", http.StatusBadRequest)
 	}
 
 }
