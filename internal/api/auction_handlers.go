3b6eae69e888a48c029e3a01957c0579b71000a7
joaomarcosg <joaomarcos.ipms@gmail.com>
2025-08-01 16:33:07 -0300

    FEAT: adds a check to confirm if the product with the given id exists

--- internal/api/auction_handlers.go
+++ internal/api/auction_handlers.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"errors"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
@@ -15,8 +16,22 @@ func (api *Api) handleSubscribeToAuction(w http.ResponseWriter, r *http.Request)
 	productID, err := uuid.Parse(rawProductID)
 	if err != nil {
 		jsonutils.EncodeJson(w, r, http.StatusBadRequest, map[string]any{
-			"message":    "invalid product id - must be a valid uuid",
-			"product_id": productID,
+			"message": "invalid product id - must be a valid uuid",
+		})
+		return
+	}
+
+	_, err = api.ProductService.GetProductById(r.Context(), productID)
+	if err != nil {
+		if errors.Is(err, services.ErrProductNotFound) {
+			jsonutils.EncodeJson(w, r, http.StatusNotFound, map[string]any{
+				"message": "no product with given id",
+			})
+			return
+		}
+
+		jsonutils.EncodeJson(w, r, http.StatusInternalServerError, map[string]any{
+			"message": "unexpected error, try again later",
 		})
 		return
 	}
