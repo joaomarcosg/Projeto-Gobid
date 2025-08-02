fe8dbe0b36f06c28dd19a7a5b24d6ec6b2b5e88a
joaomarcosg <joaomarcos.ipms@gmail.com>
2025-08-02 20:04:15 -0300

    FEAT: adds initial structure for websocket messaging

---
+++ internal/services/auctions_service.go
@@ -0,0 +1,63 @@
+package services
+
+import (
+	"context"
+	"sync"
+
+	"github.com/google/uuid"
+	"github.com/gorilla/websocket"
+)
+
+type MessageKind int
+
+const (
+	Placebid MessageKind = iota
+)
+
+type Message struct {
+	Message string
+	Kind    MessageKind
+}
+
+type AuctionLobby struct {
+	sync.Mutex
+	Rooms map[uuid.UUID]*AuctionRoom
+}
+
+type AuctionRoom struct {
+	Id         uuid.UUID
+	Context    context.Context
+	Broadcast  chan Message
+	Register   chan *Client
+	Unregister chan *Client
+	Clients    map[uuid.UUID]*Client
+
+	BidsService *BidService
+}
+
+func NewAuctionRoom(ctx context.Context, id uuid.UUID, bidsService BidService) *AuctionRoom {
+	return &AuctionRoom{
+		Id:          id,
+		Broadcast:   make(chan Message),
+		Register:    make(chan *Client),
+		Unregister:  make(chan *Client),
+		Context:     ctx,
+		BidsService: &bidsService,
+	}
+}
+
+type Client struct {
+	Room   *AuctionRoom
+	Conn   *websocket.Conn
+	Send   chan Message
+	UserID uuid.UUID
+}
+
+func NewClient(room *AuctionRoom, conn *websocket.Conn, userID uuid.UUID) *Client {
+	return &Client{
+		Room:   room,
+		Conn:   conn,
+		Send:   make(chan Message, 512),
+		UserID: userID,
+	}
+}
